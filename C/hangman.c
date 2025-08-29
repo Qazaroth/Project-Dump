e0bdf0b129d566f60413309e00e1a10a9b1127c5
Qazaroth <65128305+Qazaroth@users.noreply.github.com>
2025-08-29 11:26:56 +0800

    Update hangman.c
    Was forced to use bool...

--- C/hangman.c
+++ C/hangman.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_WORD_LENGTH 50
 #define MAX_HINT_LENGTH 100
@@ -18,11 +20,15 @@ struct WordData words[] = {
     { "human", "Our species" }
 };
 
-void displayWord(const char* word, const size_t length) {
+void displayWord(const char* word, const bool guessed[]) {
     printf("Word: ");
     
-    for (int i = 0; i < length; i++) {
-        printf(word[i]);
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (guessed[word[i] - 'a']) {
+            printf("%c ", word[i]);
+        } else {
+            printf("_ ");
+        }
     }
     printf("\n");
 }
@@ -49,16 +55,49 @@ int main() {
     size_t wordLength = strlen(word);
 
     char guessedWord[MAX_WORD_LENGTH] = { 0 };
-    int guessedLetters[26] = { 0 };
+    bool guessedLetters[26] = { false };
 
     int tries = 0;
 
-    //printf("Selected Index: %d\n", selectedWordIndex);
+    while (tries < MAX_TRIES) {
+        printf("\n");
+        displayWord(guessedWord, guessedLetters);
+        drawHangman(tries);
 
-    printf("Hanged man.\n");
-    printf("Word: %s\n", word);
-    printf("Hint: %s\n", hint);
+        char guess;
+        printf("Enter a letter: ");
+        scanf("%c", &guess);
+        guess = tolower(guess);
 
-    displayWord(word, wordLength);
-    drawHangman(tries);
+        if (guessedLetters[guess - 'a']) {
+            printf("\nYou've already guessed that letter. \nTry again.\n");
+            continue;
+        }
+
+        guessedLetters[guess - 'a'] = true;
+
+        int found = 0;
+        for (int i = 0; i < wordLength; i++) {
+            if (word[i] == guess) {
+                found = 1;
+                guessedWord[i] = guess;
+            }
+        }
+
+        if (found) {
+            printf("Good guess!\n");
+        } else {
+            printf("Sorry, letter %c is not in the word.\n", guess);
+            tries++;
+        }
+
+        if (strcmp(word, guessedWord) == 0) {
+            printf("\nCongratulations. You've guessed the word: %s", word);
+            break;
+        }
+    }
+
+    if (tries >= MAX_TRIES) {
+        printf("Sorry, you've run out of tries. The word was: %s\n", word);
+    }
 }
